418578b3691e7c6fa46df7c3b5f0219f9c31c82a
unknown <dhananjaykhairnar15@gmail.com>
2023-04-11 23:37:25 +0530

    maven pro

---
+++ src/main/java/jenkins_maven_test/Demo.java
@@ -0,0 +1,7 @@
+package jenkins_maven_test;
+
+public class Demo {
+	public static void main() {
+		System.out.println("i am dk");
+	}
+}
---
+++ src/test/java/jenkins_maven_test/jenkins_maven.java
@@ -0,0 +1,14 @@
+package jenkins_maven_test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class jenkins_maven {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
